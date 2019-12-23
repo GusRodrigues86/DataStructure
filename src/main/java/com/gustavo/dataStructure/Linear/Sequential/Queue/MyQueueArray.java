3631b657a5421762fefe8a90f0fe6edeaa2a0936
GusRodrigues <omykronbr@gmail.com>
2019-12-23 00:56:12 -0500

    Test cases and interface for queue

---
+++ src/main/java/com/gustavo/dataStructure/Linear/Sequential/Queue/MyQueueArray.java
@@ -0,0 +1,109 @@
+package com.gustavo.dataStructure.Linear.Sequential.Queue;
+
+/**
+ * The MyQueueArray uses a fixed array to hold elements on it.
+ * The size of the queue is extreme low -> 16 elements.
+ * 
+ * @author Gustavo
+ *
+ */
+public class MyQueueArray<E> implements MyQueue<E> {
+	
+	private E[] queue;
+	private int headIndex;
+	private int tailIndex;
+	private int size;
+	
+	// AF(queue, head, tailIndex) =
+	// queue != null
+	// headIndex >= 0 && tailIndex >= 0
+	
+	// Rep Invariance 
+	// queue.length == 16 && queue != null
+	// headIndex >= 0 && headIndex < 16
+	// tailIndex >= 0 && tailIndex < 16
+	// size < queue.length && size >= 0
+	
+	/**
+	 * Constructs a new Queue with max capacity of 16 elements
+	 */
+	public MyQueueArray() {
+		this(16);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private MyQueueArray(int capacity) {
+		this.queue = (E[]) new Object[capacity];
+		this.tailIndex = 0;
+		this.headIndex = 0;
+		this.size = 0;
+		checkRep();
+	}
+	
+	private void checkRep() {
+		// queue.length == 16
+		// headIndex >= 0 && headIndex < 16
+		// tailIndex >= 0 && tailIndex < 16
+		// size < queue.length && size >= 0
+		if (queue.length != 16) {
+			throw new RuntimeException("Invalid queue");
+		}
+		
+		if (!(headIndex < 16 && headIndex >= 0) || 
+			!(tailIndex < 16 && tailIndex >= 0)) {
+			throw new RuntimeException("Invalid queue");
+		}
+		
+		if (size > queue.length || size < 0) {
+			throw new RuntimeException("Invalid queue");
+		}
+	}
+
+	@Override
+	public boolean enqueue(E e) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public E dequeue() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public boolean contains(E e) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public boolean remove(E e) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return (tailIndex == headIndex && queue[headIndex] == null) ? true : false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+
+}
---
+++ src/test/java/com/gustavo/dataStructure/linear/Queue/MyQueueTests.java
@@ -0,0 +1,213 @@
+package com.gustavo.dataStructure.linear.Queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import com.gustavo.dataStructure.Linear.Sequential.Queue.MyQueue;
+
+/**
+ * @author Gustavo
+ *
+ */
+public interface MyQueueTests<E> {
+	MyQueue<E> createQueue();
+
+	List<E> elementsToTest();
+
+	E invalidElement();
+
+	/*
+	 * isEmpty case
+	 */
+
+	@Test
+	default void queueStartsEmptyAndSizeZero() {
+		MyQueue<E> queue = createQueue();
+		assertTrue(queue.isEmpty());
+		assertTrue(queue.size() == 0);
+	}
+
+	/*
+	 * Test case for queue clear()
+	 */
+
+	@Test
+	default void queueSizeIsZeroAndIsEmptyAfterClear() {
+		// assemble
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		int initialSize = queue.size();
+		// act
+		queue.clear();
+		assertTrue(queue.isEmpty(), "Queue must be empty");
+		assertTrue(initialSize > queue.size(), "Queue size before clear must be bigger than the actual size");
+		assertTrue(queue.size() == 0, "Queue size must be zero");
+
+	}
+
+	/*
+	 * Test case for enqueue
+	 */
+
+	@Test
+	default void queueSizeChangesAfterInsertion() {
+		MyQueue<E> queue = createQueue();
+		int initialSize = queue.size();
+		elementsToTest().forEach(e -> {
+			assertTrue(queue.enqueue(e), "Must return true when enqueue valid element");
+			int currentSize = queue.size();
+			assertTrue(currentSize > initialSize, "Must increase size when enqueue valid element");
+			assertFalse(queue.isEmpty(), "Can not be empty after enqueue");
+		});
+	}
+
+	@Test
+	default void queueAcceptedElementAndIncreaseSize() {
+		MyQueue<E> queue = createQueue();
+		boolean insert = queue.enqueue(elementsToTest().get(0));
+
+		assertFalse(queue.isEmpty(), "Queue must not be empty");
+		assertTrue(queue.size() == 1, "Queue size must be 1");
+		assertTrue(insert, "Must return true when adding valid element");
+	}
+
+	@Test
+	default void queueThrowNPEForInsertionOfNull() {
+		MyQueue<E> queue = createQueue();
+		assertThrows(NullPointerException.class, () -> queue.enqueue(null), "Must throew NPE when enqueuing null.");
+		assertTrue(queue.isEmpty(), "Queue must be empty");
+		assertTrue(queue.size() == 0, "Queue must have size 0");
+	}
+
+	/*
+	 * Test case for Dequeue
+	 */
+
+	@Test
+	default void queueDequeueChangeSize() {
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		int initialSize = queue.size();
+
+		queue.dequeue();
+		int finalSize = queue.size();
+
+		assertFalse(queue.isEmpty());
+		assertTrue(finalSize < initialSize,
+				"The final size must be less than: " + initialSize + " but was: " + finalSize);
+	}
+
+	@Test
+	default void queueDequeueReturnFirstElementAdded() {
+		MyQueue<E> queue = createQueue();
+		E firstElement = elementsToTest().get(0);
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		// act
+		E dequeueElement = queue.dequeue();
+		// assert
+		assertEquals(firstElement, dequeueElement, firstElement + " must be equal to " + dequeueElement);
+	}
+
+	@Test
+	default void queueDequeueReturnNullOnEmptyQueue() {
+		MyQueue<E> queue = createQueue();
+		assertTrue(queue.isEmpty());
+		assertTrue(queue.size() == 0);
+
+		// act
+		E result = queue.dequeue();
+		assertNull(result, "Must return null");
+	}
+
+	/*
+	 * Peek
+	 */
+
+	@Test
+	default void queuePeekOnEmptyQueueReturnsNull() {
+		MyQueue<E> queue = createQueue();
+		assertNull(queue.peek(), "Must be null");
+	}
+
+	@Test
+	default void queueInsertionDoesntChangeFirstItem() {
+		MyQueue<E> queue = createQueue();
+		E e = elementsToTest().get(0);
+		elementsToTest().forEach(item -> {
+			queue.enqueue(item);
+			boolean peek = queue.peek().equals(e);
+			assertTrue(peek, "Peek must be equal to true but was " + e);
+		});
+	}
+
+	/*
+	 * Contains test
+	 */
+	
+	@Test
+	default void queueContainsThrowNPEIfNullIsSupplied() {
+		assertThrows(NullPointerException.class, () -> createQueue().contains(null), "Must throw NPE if searching for null");
+	}
+
+	@Test
+	default void queueContainsReturnFalseWhenElementIsNotOnTheQueue() {
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		boolean contains = queue.contains(invalidElement());
+		assertFalse(contains, "Queue must not have the element we are searching for");
+	}
+
+	@Test
+	default void queueContainsReturnTrueWhenElementIsOnTheQueue() {
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		E toRemove = elementsToTest().get(new Random().nextInt(elementsToTest().size()));
+		// act
+		boolean remove = queue.contains(toRemove);
+		// assert
+		assertTrue(remove, "Must return true when the element is on the Queue");
+	}
+
+	/*
+	 * Removal test
+	 */
+
+	@Test
+	default void queueThrowNPEForRemoveOfNull() {
+		MyQueue<E> queue = createQueue();
+		assertThrows(NullPointerException.class, () -> queue.remove(null));
+		assertTrue(queue.isEmpty());
+		assertTrue(queue.size() == 0);
+	}
+
+	@Test
+	default void removeElementThatIsNotOnTheQueueReturnsFalse() {
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		E e = invalidElement();
+
+		boolean result = queue.remove(e);
+
+		assertFalse(result);
+	}
+
+	@Test
+	default void removeElementThatIsOnTheQueueReturnsTrue() {
+		MyQueue<E> queue = createQueue();
+		elementsToTest().forEach(e -> queue.enqueue(e));
+		E e = elementsToTest().get(new Random().nextInt(elementsToTest().size()));
+
+		boolean result = queue.remove(e);
+
+		assertTrue(result);
+	}
+
+}
