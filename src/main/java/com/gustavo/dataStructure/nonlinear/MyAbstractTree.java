aeaf071a841a27b147eca68bd712dd9e68254d63
GusRodrigues <omykronbr@gmail.com>
2020-01-18 15:37:11 -0500

    tree ds

---
+++ src/main/java/com/gustavo/dataStructure/nonlinear/MyAbstractTree.java
@@ -0,0 +1,20 @@
+package com.gustavo.dataStructure.nonlinear;
+
+/**
+ * My MyAbstractTree is an implementation of the ADT {@link MyTree}
+ * 
+ * it declares basic operations such as the structure of the Nodes of the Tree
+ * 
+ * 
+ * @author Gustavo
+ *
+ */
+public class MyAbstractTree<E> implements MyTree<E> {
+
+	@Override
+	public E getElement() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Not implemented");
+	}
+	
+}
---
+++ src/main/java/com/gustavo/dataStructure/nonlinear/MyTree.java
@@ -0,0 +1,17 @@
+package com.gustavo.dataStructure.nonlinear;
+
+/**
+ * The Tree ADT for implementation
+ * 
+ * @author Gustavo
+ *
+ */
+public interface MyTree<E> {
+	/**
+	 * Return the element at this position
+	 * 
+	 * @return The element at this position
+	 */
+	E getElement();
+	
+}
