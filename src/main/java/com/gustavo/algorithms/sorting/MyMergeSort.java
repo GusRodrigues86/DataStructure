e0198f81ebdee27a4fc5d7d1a9c3a25eca9184d5
GusRodrigues <omykronbr@gmail.com>
2020-01-18 19:56:29 -0500

    Merge sort test cases and implementation for ints, double, and long

--- src/main/java/com/gustavo/algorithms/sorting/MyMergeSort.java
+++ src/main/java/com/gustavo/algorithms/sorting/MyMergeSort.java
@@ -1,16 +1,199 @@
 package com.gustavo.algorithms.sorting;
 
 /**
+ * Implementation of merge-sort for some primitive types array.
+ * 
+ * The overload of the sort method is required due to the generic constraint for
+ * primitive types in Java.
+ * 
  * @author Gustavo
  *
  */
 public class MyMergeSort {
+
 	/**
-	 * Sorts in ascending order the elements of the array
+	 * Sorts an integer array in ascending order
+	 * 
 	 * @param array to be sorted
 	 * @return the permuted array in ascending order
 	 */
-	public static Object[] sort(Object[] array) {
-		return new Integer[]{1};
+	public static int[] sort(int[] arr) {
+		int n = arr.length;
+
+		// base case
+		if (n == 1) {
+			return arr;
+		}
+
+		int[] arrL = new int[n / 2];
+		// populates the array
+		for (int i = 0; i < arrL.length; i++) {
+			arrL[i] = arr[i];
+		}
+
+		int[] arrR = new int[n - (n / 2)]; // Accommodates odd sized arrays
+		// populates the array
+		for (int i = 0; i < arrR.length; i++) {
+			arrR[i] = arr[i + (n / 2)];
+		}
+
+		return merge(sort(arrL), sort(arrR));
+	}
+
+	/**
+	 * Merges two arrays in sorted manner
+	 * 
+	 * @param arrL first array to be merged
+	 * @param arrR second array to be merged
+	 * @return the merged array in ascending sorting manner
+	 */
+	private static int[] merge(int[] arrL, int[] arrR) {
+
+		int[] merged = new int[arrL.length + arrR.length];
+		// l -> arrL index and r -> arrR index
+		int l = 0, r = 0;
+		for (int i = 0; i < merged.length; i++) {
+
+			if (l >= arrL.length) {
+				merged[i] = arrR[r];
+				r++;
+			} else if (r >= arrR.length) {
+				merged[i] = arrL[l];
+				l++;
+			} else if (arrL[l] <= arrR[r]) {
+				merged[i] = arrL[l];
+				l++;
+			}
+			// if (arrR[r] <= arrL[l]
+			else {
+				merged[i] = arrR[r];
+				r++;
+			}
+		}
+		return merged;
+	}
+
+	/**
+	 * Sorts a long array in ascending order
+	 * 
+	 * @param array to be sorted
+	 * @return the permuted array in ascending order
+	 */
+	public static long[] sort(long[] arr) {
+		int n = arr.length;
+
+		// base case
+		if (n == 1) {
+			return arr;
+		}
+
+		long[] arrL = new long[n / 2];
+		// populates the array
+		for (int i = 0; i < arrL.length; i++) {
+			arrL[i] = arr[i];
+		}
+
+		long[] arrR = new long[n - (n / 2)]; // Accommodates odd sized arrays
+		// populates the array
+		for (int i = 0; i < arrR.length; i++) {
+			arrR[i] = arr[i + (n / 2)];
+		}
+
+		return merge(sort(arrL), sort(arrR));
+	}
+
+	/**
+	 * Merges two arrays in sorted manner
+	 * 
+	 * @param arrL first array to be merged
+	 * @param arrR second array to be merged
+	 * @return the merged array in ascending sorting manner
+	 */
+	private static long[] merge(long[] arrL, long[] arrR) {
+
+		long[] merged = new long[arrL.length + arrR.length];
+		// l -> arrL index and r -> arrR index
+		int l = 0, r = 0;
+		for (int i = 0; i < merged.length; i++) {
+
+			if (l >= arrL.length) {
+				merged[i] = arrR[r];
+				r++;
+			} else if (r >= arrR.length) {
+				merged[i] = arrL[l];
+				l++;
+			} else if (arrL[l] <= arrR[r]) {
+				merged[i] = arrL[l];
+				l++;
+			}
+			// if (arrR[r] <= arrL[l]
+			else {
+				merged[i] = arrR[r];
+				r++;
+			}
+		}
+		return merged;
+	}
+
+	/**
+	 * Sorts a double array in ascending order
+	 * 
+	 * @param array to be sorted
+	 * @return the permuted array in ascending order
+	 */
+	public static double[] sort(double[] arr) {
+		int n = arr.length;
+
+		// base case
+		if (n == 1) {
+			return arr;
+		}
+
+		double[] arrL = new double[n / 2];
+		// populates the array
+		for (int i = 0; i < arrL.length; i++) {
+			arrL[i] = arr[i];
+		}
+
+		double[] arrR = new double[n - (n / 2)]; // Accommodates odd sized arrays
+		// populates the array
+		for (int i = 0; i < arrR.length; i++) {
+			arrR[i] = arr[i + (n / 2)];
+		}
+
+		return merge(sort(arrL), sort(arrR));
+	}
+
+	/**
+	 * Merges two arrays in sorted manner
+	 * 
+	 * @param arrL first array to be merged
+	 * @param arrR second array to be merged
+	 * @return the merged array in ascending sorting manner
+	 */
+	private static double[] merge(double[] arrL, double[] arrR) {
+
+		double[] merged = new double[arrL.length + arrR.length];
+		// l -> arrL index and r -> arrR index
+		int l = 0, r = 0;
+		for (int i = 0; i < merged.length; i++) {
+
+			if (l >= arrL.length) {
+				merged[i] = arrR[r];
+				r++;
+			} else if (r >= arrR.length) {
+				merged[i] = arrL[l];
+				l++;
+			} else if (arrL[l] <= arrR[r]) {
+				merged[i] = arrL[l];
+				l++;
+			}
+			// if (arrR[r] <= arrL[l]
+			else {
+				merged[i] = arrR[r];
+				r++;
+			}
+		}
+		return merged;
 	}
 }
--- src/test/java/com/gustavo/algorithms/sorting/TestSort.java
+++ src/test/java/com/gustavo/algorithms/sorting/TestSort.java
@@ -1,29 +1,118 @@
 package com.gustavo.algorithms.sorting;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 import static com.gustavo.algorithms.sorting.MyMergeSort.*; 
 /**
- * Test cases for Sorting
+ * Test cases for Sorting integer arrays.
  * 
  * @author Gustavo
  *
  */
 public class TestSort {
 	
-
+	private int[] veryLargeArray = largeArray();
+	
+	private static int[] largeArray() {
+		int[] array = new int[1000000];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Random().nextInt(2500000);
+		}
+		return array;
+	}
 	
 	@Test
 	public void testArrayOfOneElement() {
 		// assemble
-		Integer[] array = {1};
+		int[] array = {1};
 		// act
-		Integer[] answer = (Integer[]) sort(array); // safecast
+		int[] answer = sort(array); 
 		// assert
 		assertEquals(1, answer.length);
 		assertEquals(1, answer[0]);
 	}
+	@Test
+	public void testArrayOfTwoElement() {
+		// assemble
+		int[] array = {2,1};
+		// act
+		int[] answer = sort(array); 
+		// assert
+		assertEquals(2, answer.length);
+		assertEquals(1, answer[0]);
+		assertTrue(answer[0] < answer[1], "first element must be less than the second, but was: " + answer[0] + " and " + answer[1]);
+	}
+	@Test
+	public void testArrayOfThreeElement() {
+		// assemble
+		int[] array = {2,1,4};
+		// act
+		int[] answer = sort(array); 
+		// assert
+		assertEquals(3, answer.length);
+		assertEquals(1, answer[0]);
+		for (int i = 1; i < array.length; i++) {
+			int before = answer[i-1];
+			int after = answer[i];
+			String assertion = before + " must be <= than " + after;
+			assertTrue(before < after, assertion);			
+		}
+	}
 	
+	@Test
+	public void testArrayOf50Element() {
+		// assemble
+		int[] array = new int[50];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Random().nextInt(2500);
+		}
+		// act
+		int[] answer = sort(array); 
+		// assert
+		
+		for (int i = 1; i < array.length; i++) {
+			int before = answer[i-1];
+			int after = answer[i];
+			String assertion = before + " must be <= than " + after;
+			assertTrue(before <= after, assertion);			
+		}
+	}
 	
+	@Test
+	public void testArrayOf50000Element() {
+		// assemble
+		int[] array = new int[50000];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Random().nextInt(250000);
+		}
+		// act
+		int[] answer = sort(array); 
+		// assert
+		
+		for (int i = 1; i < array.length; i++) {
+			int before = answer[i-1];
+			int after = answer[i];
+			String assertion = before + " must be <= than " + after;
+			assertTrue(before <= after, assertion);			
+		}
+	}
+	
+	@Test
+	public void testArrayOfAMillionElement() {
+		
+		// act
+		int[] answer = sort(veryLargeArray); 
+		// assert
+		
+		for (int i = 1; i < veryLargeArray.length; i++) {
+			int before = answer[i-1];
+			int after = answer[i];
+			String assertion = before + " must be <= than " + after;
+			assertTrue(before <= after, assertion);			
+		}
+	}
 }
